9f777ce275cbde11b320cd817faebb8ed0d00be6
Siva <sivaprasadtppba@gmail.com>
2022-10-22 02:58:04 +0530

    success and incorrect message displayed

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ let handleClick = (value) => {
 let handleSubmit = () => {
   let value = document.getElementById("display").value;
   if (value === "1234") {
-    alert("Success");
+    document.getElementById("message").classList.remove('failed');
+    document.getElementById("message").classList.add('success');
+    document.getElementById("message").innerHTML = "Success";
   } else {
-    alert("Incorrect Pin");
+    document.getElementById("message").classList.remove('success');
+    document.getElementById("message").classList.add('failed');
+    document.getElementById("message").innerHTML = "Incorrect";
+
   }
   handleClear();
 };
